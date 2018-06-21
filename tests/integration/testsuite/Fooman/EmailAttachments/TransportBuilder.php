3b33b8ec0debcd9407d72d59fe33603a0aebccf2
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2018-06-21 19:58:55 +1200

    Reset tests to send actual emails

---
+++ tests/integration/testsuite/Fooman/EmailAttachments/TransportBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_EmailAttachments
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Fooman\EmailAttachments;
+
+class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
+{
+
+}
