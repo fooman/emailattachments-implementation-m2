4326f149934e28704ba7b6edab8ee5b376f7e8cc
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2018-06-21 19:04:27 +1200

    Change approach to detect email that is being sent

---
+++ src/Plugin/TransportBuilder.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_EmailAttachments
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Fooman\EmailAttachments\Plugin;
+
+class TransportBuilder
+{
+
+
+    private $nextEmail;
+
+    public function __construct(
+        \Fooman\EmailAttachments\Model\NextEmailInfo $nextEmailInfo
+    ) {
+        $this->nextEmail = $nextEmailInfo;
+    }
+
+    public function beforeSetTemplateIdentifier(
+        \Magento\Framework\Mail\Template\TransportBuilder $subject,
+        $templateIdentifier
+    ) {
+        $this->nextEmail->setTemplateIdentifier($templateIdentifier);
+    }
+
+    public function beforeSetTemplateVars(
+        \Magento\Framework\Mail\Template\TransportBuilder $subject,
+        $templateVars
+    ) {
+        $this->nextEmail->setTemplateVars($templateVars);
+    }
+
+    public function aroundGetTransport(
+        \Magento\Framework\Mail\Template\TransportBuilder $subject,
+        \Closure $proceed
+    ) {
+        $mailTransport = $proceed();
+        $this->reset();
+        return $mailTransport;
+    }
+
+    private function reset()
+    {
+        $this->nextEmail->setTemplateIdentifier(null);
+        $this->nextEmail->setTemplateVars(null);
+    }
+}
